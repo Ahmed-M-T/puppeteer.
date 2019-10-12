7be4542d5cd27be244edf4c29556ac0de2252c94
Ahmed-M-T <ahmed.mtaha47@gamil.com>
2019-10-12 21:27:38 +0200

    secound commit

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const puppeteer = require('puppeteer');
-var count =1;
-var interval = setInterval(register, 10*1000);
+var faker = require('faker');
 
- function register(){
+var count =1;
+var interval = setInterval(start, 10*1000);
+function start(){
+    var firstName = faker.name.firstName(); 
+    var lastname =faker.name.lastName();
+var userName = faker.internet.userName(); 
+var password =faker.internet.password();
+register(firstName,lastname,userName,password);
+
+}
+ function register(fname,lname,userName,password){
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://accounts.google.com/signup/v2/webcreateaccount?flowName=GlifWebSignIn&flowEntry=SignUp');
   
-await page.type('#firstName', 'ahmed');
-await page.type('#lastName', 'taha');
-await page.type('#username', 'ahmed.mtaha53');
-await page.type('#passwd > div.aCsJod.oJeWuf > div > div.Xb9hP > input', '$!#@ahmed123');
-await page.type('#confirm-passwd > div.aCsJod.oJeWuf > div > div.Xb9hP > input', '$!#@ahmed123');
-await page.screenshot({path: `example${count++}.png`});
+await page.type('#firstName', fname);
+await page.type('#lastName', lname);
+await page.type('#username', userName);
+await page.type('#passwd > div.aCsJod.oJeWuf > div > div.Xb9hP > input', password);
+await page.type('#confirm-passwd > div.aCsJod.oJeWuf > div > div.Xb9hP > input', password);
+await page.screenshot({path: `page${count}.png`});
 if(count == 10)
 clearInterval(interval);
 
@@ -24,7 +33,7 @@ await Promise.all([
 
 
 console.log('New Page URL:', page.url());
-  await page.screenshot({path: 'example2.png'});
+  await page.screenshot({path: `page2${count++}.png`});
 
   await browser.close();
 })();
